bfc6135f1b491e4bac243b1385c4ee45584abc29
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-09-17 23:48:28 +0900

    presence and absence

--- pb2q/operators/__init__.py
+++ pb2q/operators/__init__.py
@@ -1,13 +1,15 @@
 """Sympy representations of operators."""
 
 from .field import FieldOperator, apply_field_op
-from .particle import Control
+from .particle import Control, PresenceProjection, AbsenceProjection
 from .symm import ParticleSwap, StepAntisymmetrizer, StepSymmetrizer
 
 __all__ = [
     'FieldOperator',
     'apply_field_op',
     'Control',
+    'PresenceProjection',
+    'AbsenceProjection',
     'ParticleSwap',
     'StepAntisymmetrizer',
     'StepSymmetrizer'
