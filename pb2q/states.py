2406194baa2d85a928110982fc21769395c1b357
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2024-09-20 12:10:38 +0900

    ProductState working

--- pb2q/states.py
+++ pb2q/states.py
@@ -43,6 +43,10 @@ class UniverseState(ProductState):
         return r'\otimes'.join(fr'\llbracket {arg._latex(printer, *args)} \rrbracket'
                                for arg in self.args)
 
+    @property
+    def field(self):
+        return self.args
+
 
 class UniverseKet(UniverseState, ProductKet):
     """TensorProduct of FieldKets."""
@@ -92,6 +96,10 @@ class FieldState(ProductState):
         #                        for arg in self.args)
         return r'\otimes'.join(arg._latex(printer, *args) for arg in self.args)
 
+    @property
+    def particle(self):
+        return self.args
+
 
 class FieldKet(FieldState, ProductKet):
     """TensorProduct of ParticleKets."""
@@ -138,16 +146,16 @@ class ParticleState(StateBase, TensorProduct):
             return QExpr.__new__(cls, None)
 
         args = sympify(args)
-        if len(args) == 1 and isinstance(args[0], TensorProduct):
-            # Type-casting form (Single TensorProduct argument)
-            args = args[0].args
-
         if len(args) == 2:
             pcls = cls.momentum_state_class()
             qcls = cls.qnumber_state_class()
-            if isinstance(args[0], (Integral, Sequence, Tuple)):
+            if isinstance(args[0], Integral):
+                args = (pcls(args[0]), args[1])
+            elif isinstance(args[0], (Sequence, Tuple)):
                 args = (pcls(*args[0]), args[1])
-            if isinstance(args[1], (Sequence, Tuple)):
+            if isinstance(args[1], Integral):
+                args = (args[0], qcls(args[1]))
+            elif isinstance(args[1], (Sequence, Tuple)):
                 args = (args[0], qcls(*args[1]))
 
             if (isinstance(args[0], pcls) and isinstance(args[1], qcls)):
@@ -159,10 +167,19 @@ class ParticleState(StateBase, TensorProduct):
     def default_args(cls):
         return None
 
+    @property
+    def dual(self):
+        """Return the dual state of this one."""
+        if self.args[0] is None:
+            dual_args = (None,)
+        else:
+            dual_args = tuple(arg.dual for arg in self.args)
+        return self.dual_class()._new_rawargs(self.hilbert_space, *dual_args)
+
     def _print_contents(self, printer, *args):
         if self.args[0] is None:
             return '_O_'
-        return ';'.join(arg._print_contents(printer, *args) for arg in self.args)
+        return ':'.join(arg._print_contents(printer, *args) for arg in self.args)
 
     def _print_contents_pretty(self, printer, *args):
         if self.args[0] is None:
@@ -171,23 +188,22 @@ class ParticleState(StateBase, TensorProduct):
             return prettyForm('_O_')
 
         pform = self.args[0]._print_contents_pretty(printer, *args)
-        pform = prettyForm(*pform.right(','))
+        pform = prettyForm(*pform.right(':'))
         pform = prettyForm(*pform.right(self.args[1]._print_contents_pretty(printer, *args)))
         return pform
 
     def _print_contents_latex(self, printer, *args):
         if self.args[0] is None:
             return r'\Omega'
-        return '; '.join(arg._print_contents_latex(printer, *args) for arg in self.args)
+        return ': '.join(arg._print_contents_latex(printer, *args) for arg in self.args)
 
-    def _sympystr(self, printer, *args):
-        return super(TensorProduct, self)._sympystr(printer, *args)  # pylint: disable
+    @property
+    def momentum(self):
+        return self.args[0]
 
-    def _pretty(self, printer, *args):
-        return super(TensorProduct, self)._pretty(printer, *args)
-
-    def _latex(self, printer, *args):
-        return super(TensorProduct, self)._latex(printer, *args)
+    @property
+    def qnumber(self):
+        return self.args[1]
 
 
 class ParticleKet(ParticleState, KetBase):
@@ -238,50 +254,40 @@ class ParticleBra(ParticleState, BraBase):
         return QNumberBra
 
 
-class MomentumState(ProductState):
-    """Generic momentum state."""
-    _label_separator = ','
-
-    def __new__(cls, *args):
-        args = sympify(args)
-        if len(args) == 1 and isinstance(args[0], Integral):
-            args = (args,)
-
-        return super().__new__(cls, *args)
-
-
-class MomentumKet(MomentumState, ProductKet):
-    """Momentum ket."""
-    @classmethod
-    def dual_class(cls):
-        return MomentumBra
-
-    @classmethod
-    def component_class(cls):
-        return OrthogonalKet
-
-
-class MomentumBra(MomentumState, ProductBra):
-    """Momentum ket."""
-    @classmethod
-    def dual_class(cls):
-        return MomentumKet
-
-    @classmethod
-    def component_class(cls):
-        return OrthogonalBra
-
-
 class QNumberState(ProductState):
     """Generic quantum number state."""
     _label_separator = ';'
 
     def __new__(cls, *args):
-        args = sympify(args)
-        if len(args) == 1 and isinstance(args[0], Integral):
-            args = (args,)
+        comp_cls = cls.component_class()
+        statified = []
+        for arg in args:
+            # pylint: disable-next=isinstance-second-argument-not-valid-type
+            if isinstance(arg, comp_cls):
+                statified.append(arg)
+            else:
+                statified.append(comp_cls(arg))  # pylint: disable=not-callable
 
-        return super().__new__(cls, *args)
+        return super().__new__(cls, *statified)
+
+    def _print_contents(self, printer, *args):
+        return self._label_separator.join(arg._print_contents(printer, *args) for arg in self.args)
+
+    def _print_contents_pretty(self, printer, *args):
+        pform = self.args[0]._print_contents_pretty(printer, *args)
+        for arg in self.args[1:]:
+            pform = prettyForm(*pform.right(self._label_separator))
+            pform = prettyForm(*pform.right(arg._print_contents_pretty(printer, *args)))
+        return pform
+
+    def _print_contents_latex(self, printer, *args):
+        return self._label_separator.join(
+            arg._print_contents_latex(printer, *args) for arg in self.args
+        )
+
+    @property
+    def component(self):
+        return self.args
 
 
 class QNumberKet(QNumberState, ProductKet):
@@ -300,6 +306,45 @@ class QNumberBra(QNumberState, ProductBra):
     @classmethod
     def dual_class(cls):
         return QNumberKet
+
+    @classmethod
+    def component_class(cls):
+        return OrthogonalBra
+
+
+class MomentumState(QNumberState):
+    """Generic momentum state."""
+    _label_separator = ','
+
+    @property
+    def x(self):
+        return self.args[0]
+
+    @property
+    def y(self):
+        return self.args[1]
+
+    @property
+    def z(self):
+        return self.args[2]
+
+
+class MomentumKet(MomentumState, ProductKet):
+    """Momentum ket."""
+    @classmethod
+    def dual_class(cls):
+        return MomentumBra
+
+    @classmethod
+    def component_class(cls):
+        return OrthogonalKet
+
+
+class MomentumBra(MomentumState, ProductBra):
+    """Momentum ket."""
+    @classmethod
+    def dual_class(cls):
+        return MomentumKet
 
     @classmethod
     def component_class(cls):
--- pb2q/sympy/product_state.py
+++ pb2q/sympy/product_state.py
@@ -11,19 +11,21 @@ class ProductState(StateBase, TensorProduct):
 
     def __new__(cls, *args):
         args = sympify(args)
-        if len(args) == 1 and isinstance(args[0], TensorProduct):
-            # Type-casting form (Single TensorProduct argument)
-            args = args[0].args
-
         # pylint: disable-next=isinstance-second-argument-not-valid-type
         if cls.component_class and not all(isinstance(arg, cls.component_class()) for arg in args):
-            raise ValueError(f'Components of {cls.__name__} must be {cls.component_class.__name__}')
+            raise ValueError(f'Components of {cls.__name__} must be'
+                             f' {cls.component_class().__name__}')
 
         return QExpr.__new__(cls, *args)
 
     @classmethod
     def component_class(cls) -> type[StateBase]:
         return None
+
+    @property
+    def dual(self):
+        """Return the dual state of this one."""
+        return self.dual_class()._new_rawargs(self.hilbert_space, *[arg.dual for arg in self.args])
 
 
 class ProductKet(ProductState, KetBase):
@@ -35,6 +37,7 @@ class ProductKet(ProductState, KetBase):
     def _eval_innerproduct(self, bra, **hints):
         # TODO: can use Hilbert space check if that's implemented
         if isinstance(bra, ProductBra):
+            print('bra is product')
             if len(bra.args) != len(self.args):
                 raise ValueError('Cannot multiply a product ket that has a different number of'
                                  ' components.')
